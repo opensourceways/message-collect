7087ada26b27a8c151d2ea212244ba60df31e48c
shishupei <shishupei10@163.com>
2024-04-23 14:58:06 +0800

    fedora message 转发到kafka

--- test_environment/fed_msg/main.py
+++ test_environment/fed_msg/main.py
@@ -1,6 +1,36 @@
+import json
+from dataclasses import dataclass
 from fedora_messaging import api, config
+from kafka import KafkaProducer
+from typing import Dict
+
+
+@dataclass
+class Event:
+    Key: str
+    Header: Dict[str, str]
+    Body: object
+
+
+# 创建生产者
+producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version=(2, 8, 1))
+
+# Kafka 主题
+topic = 'eur_build_raw'
 
 config.conf.setup_logging()
+
+
+def json_marshal(obj):
+    # 将对象编码为 JSON 格式的字符串
+    json_string = json.dumps(
+        obj,
+        default=lambda o: o.__dict__,
+        sort_keys=True,
+        indent=4)
+    # 将字符串转换为字节数组
+    json_bytes = json_string.encode('utf-8')
+    return json_bytes
 
 
 def print_eur_message(message):
@@ -8,11 +38,14 @@ def print_eur_message(message):
     if message.topic == "org.fedoraproject.prod.copr.build.end" or message.topic == "org.fedoraproject.prod.copr.build.start":
         print("******** Received eur event: ********\n")
         print(message)
+
+        producer.send(topic, json_marshal(message))
     else:
         print("******** Message is not triggered by eur, skipped ********")
 
 
 def main():
     api.consume(callback=print_eur_message)
+
 
 main()
